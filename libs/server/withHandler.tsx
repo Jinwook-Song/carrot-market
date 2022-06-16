68a79ee285b39c15898713f9740fe3aac057449c
jinwook song <wlsdnr129@naver.com>
2022-06-16 20:20:43 +0900

    api handler

---
+++ libs/server/withHandler.tsx
@@ -0,0 +1,20 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+
+export default function withHandler(
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  handler: NextApiHandler
+) {
+  return async function (req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== method) {
+      return res.status(405).end();
+    }
+    try {
+      await handler(req, res);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error });
+    }
+  };
+}
+
+// NextJS는 withHandler가 Return하는것을 실행
