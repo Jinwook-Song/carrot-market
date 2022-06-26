3dfadd98bfa8ff3395b641645a70ffcd2934af8b
jinwook song <wlsdnr129@naver.com>
2022-06-26 20:18:21 +0900

    toggle backend fav checked

--- pages/api/products/[id]/fav.ts
+++ pages/api/products/[id]/fav.ts
@@ -7,6 +7,39 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+
+  const alreadyExists = await client.fav.findFirst({
+    where: {
+      productId: +id.toString(),
+      userId: user?.id,
+    },
+  });
+
+  if (alreadyExists) {
+    // DELETE: deleteMany와는 다르게 Unique 속성으로만 삭제 가능
+    await client.fav.delete({
+      where: {
+        id: alreadyExists.id,
+      },
+    });
+  } else {
+    await client.fav.create({
+      data: {
+        user: {
+          connect: { id: user?.id },
+        },
+        product: {
+          connect: {
+            id: +id.toString(),
+          },
+        },
+      },
+    });
+  }
   res.json({
     ok: true,
   });
