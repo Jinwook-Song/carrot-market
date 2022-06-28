f7c7e651afcc2a7ed376efd46c7293d95ea0d947
jinwook song <wlsdnr129@naver.com>
2022-06-29 00:26:10 +0900

    Post Detail API

---
+++ pages/api/posts/[id]/index.ts
@@ -0,0 +1,61 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import client from '@libs/server/client';
+import { withApiSession } from '@libs/server/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = req;
+  const post = await client.post.findUnique({
+    where: {
+      id: +id.toString(),
+    },
+    include: {
+      user: {
+        select: {
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        select: {
+          id: true,
+          answer: true,
+          createdAt: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          wondering: true,
+        },
+      },
+    },
+  });
+
+  // 모든 API에 대해서도 처리 해줘야함
+  if (!post) res.status(404).json({ ok: false, error: 'Post not found.' });
+  res.json({
+    ok: true,
+    post,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ['GET'],
+    isPrivate: true,
+    handler,
+  })
+);
