0f13dc37ea7efb682c8fca258d55448da9fe7f79
jinwook song <wlsdnr129@naver.com>
2022-06-30 20:23:32 +0900

    post answer

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -3,11 +3,12 @@ import Layout from '@components/layout';
 import TextArea from '@components/textarea';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
-import { Post, User } from 'prisma/prisma-client';
+import { Answer, Post, User } from 'prisma/prisma-client';
 import Link from 'next/link';
 import { useEffect } from 'react';
 import useMutation from '@libs/client/useMutation';
 import { cls } from '@libs/client/utils';
+import { useForm } from 'react-hook-form';
 
 interface IExtendedPost extends Post {
   user: {
@@ -36,20 +37,26 @@ interface IComuunityPostResponse {
   isWondering: boolean;
 }
 
+interface IAnswerForm {
+  answer: string;
+}
+
+interface IAnswerResponse {
+  ok: boolean;
+  answer: Answer;
+}
+
 const CommunityPostDetail: NextPage = () => {
-  const router = useRouter();
   const { query } = useRouter();
+  const { register, getValues, handleSubmit, reset } = useForm<IAnswerForm>();
+
   const { data, mutate } = useSWR<IComuunityPostResponse>(
     query.id ? `/api/posts/${query.id}` : null
   );
 
-  useEffect(() => {
-    if (data && !data.ok) {
-      router.replace('/community');
-    }
-  }, [data, router]);
-
-  const [wonder] = useMutation(`/api/posts/${query.id}/wonder`);
+  const [wonder, { loading }] = useMutation(`/api/posts/${query.id}/wonder`);
+  const [sendAnswer, { data: answerData, loading: answerLoading }] =
+    useMutation<IAnswerResponse>(`/api/posts/${query.id}/answers`);
   const onWonderClick = () => {
     if (!data) return;
     mutate(
@@ -69,8 +76,20 @@ const CommunityPostDetail: NextPage = () => {
         },
       false
     );
-    wonder({});
+    if (!loading) {
+      wonder({});
+    }
   };
+
+  const onAnswerSubmit = () => {
+    const answer = getValues('answer');
+    if (answerLoading) return;
+    sendAnswer({ answer });
+  };
+
+  useEffect(() => {
+    if (answerData && answerData.ok) reset();
+  }, [answerData, reset]);
 
   return (
     <Layout canGoBack>
@@ -82,7 +101,7 @@ const CommunityPostDetail: NextPage = () => {
           <div className='w-10 h-10 rounded-full bg-slate-300' />
           <div>
             <p className='text-sm font-medium text-gray-700'>
-              {data?.post.user.name}
+              {data?.post?.user.name}
             </p>
             <Link href={`/users/profiles/${data?.post?.userId}`}>
               <a className='text-xs font-medium text-gray-500'>
@@ -94,7 +113,7 @@ const CommunityPostDetail: NextPage = () => {
         <div>
           <div className='mt-2 px-4 text-gray-700'>
             <span className='text-orange-500 font-medium'>Q.</span>
-            {data?.post.question}
+            {data?.post?.question}
           </div>
           <div className='flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px]  w-full'>
             <button
@@ -118,7 +137,7 @@ const CommunityPostDetail: NextPage = () => {
                   d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
                 ></path>
               </svg>
-              <span>궁금해요 {data?.post._count.wondering}</span>
+              <span>궁금해요 {data?.post?._count?.wondering}</span>
             </button>
             <span className='flex space-x-2 items-center text-sm'>
               <svg
@@ -135,12 +154,12 @@ const CommunityPostDetail: NextPage = () => {
                   d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'
                 ></path>
               </svg>
-              <span>답변 {data?.post._count.answers}</span>
+              <span>답변 {data?.post?._count?.answers}</span>
             </span>
           </div>
         </div>
         <div className='px-4 my-5 space-y-5'>
-          {data?.post.answers.map((answer) => (
+          {data?.post?.answers?.map((answer) => (
             <div key={answer.id} className='flex items-start space-x-3'>
               <div className='w-8 h-8 bg-slate-200 rounded-full' />
               <div>
@@ -155,16 +174,17 @@ const CommunityPostDetail: NextPage = () => {
             </div>
           ))}
         </div>
-        <div className='px-4'>
+        <form onSubmit={handleSubmit(onAnswerSubmit)} className='px-4'>
           <TextArea
+            register={register('answer', { required: true, minLength: 5 })}
             name='description'
             placeholder='Answer this question!'
             required
           />
           <button className='mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none '>
-            Reply
+            {answerLoading ? 'Loading...' : 'Reply'}
           </button>
-        </div>
+        </form>
       </div>
     </Layout>
   );
