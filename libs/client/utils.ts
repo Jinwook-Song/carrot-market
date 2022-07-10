befcf6e8dadad0ee8dce928ef8802200e21aff6e
jinwook song <wlsdnr129@naver.com>
2022-07-10 20:31:58 +0900

    reviews

--- libs/client/utils.ts
+++ libs/client/utils.ts
@@ -5,3 +5,7 @@ export function cls(...classnames: string[]) {
 export function fetcher(url: string) {
   return fetch(url).then((response) => response.json());
 }
+
+export const EMAIL_VALIDATION_CHECK = new RegExp(
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+);
