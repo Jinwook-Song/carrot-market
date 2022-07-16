56ef0e45b9b24817d2132093ae2b1fb648410746
jinwook song <wlsdnr129@naver.com>
2022-07-16 17:08:58 +0900

    prisma seeding

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from 'prisma/prisma-client';
+
+const client = new PrismaClient();
+
+async function main() {
+  new Array(200).fill(0).forEach(async (_, idx) => {
+    await client.stream.create({
+      data: {
+        name: String(idx),
+        description: String(idx),
+        price: idx,
+        user: {
+          connect: {
+            id: 10,
+          },
+        },
+      },
+    });
+    console.log(`${idx}/200`);
+  });
+}
+
+main()
+  .catch((e) => console.log(e))
+  .finally(() => client.$disconnect());
